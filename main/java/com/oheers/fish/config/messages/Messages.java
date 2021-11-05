d728bb21cf367e21a330b671ab8a637e711e4dd7
ThrRip <54434330+ThrRip@users.noreply.github.com>
2021-11-05 15:58:24 +0800

    Merge branch 'master' of https://github.com/Oheers/EvenMoreFish into Oheers-master

--- main/java/com/oheers/fish/config/messages/Messages.java
+++ main/java/com/oheers/fish/config/messages/Messages.java
@@ -149,7 +149,7 @@ public class Messages {
     }
 
     public String competitionNotRunning() {
-        return getErrorPrefix() + "现在没有竞争对手";
+        return getErrorPrefix() + config.getString("no-competition-running");
     }
 
     public String getNotEnoughPlayers() {
